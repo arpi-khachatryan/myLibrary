b81b913768ff508af586f5730e1c9fb2f56be6d9
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-09 06:37:01 +0400

    AddUserServlet

---
+++ src/main/java/servlet/AddUserServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import manager.UserManager;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = "/users/add")
+public class AddUserServlet extends HttpServlet {
+    private UserManager userManager = new UserManager();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/addUser.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        if (userManager.getUserByEmail(email) != null) {
+            req.setAttribute("msg", "User already exists");
+            req.getRequestDispatcher("/WEB-INF/addUser.jsp").forward(req, resp);
+        } else {
+            String name = req.getParameter("name");
+            String surname = req.getParameter("surname");
+            String password = req.getParameter("password");
+            User user = User.builder()
+                    .name(name)
+                    .surname(surname)
+                    .email(email)
+                    .password(password)
+                    .build();
+            userManager.add(user);
+            resp.sendRedirect("/login");
+        }
+    }
+}
