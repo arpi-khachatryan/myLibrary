4b549b8f297b29fc6b63ea5023f9e97a1b6de737
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-09 06:48:50 +0400

    UserFilter

--- src/main/java/filter/UserFilter.java
+++ src/main/java/filter/UserFilter.java
@@ -8,26 +8,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebServlet(urlPatterns = "/books/**")
-//public class UserFilter implements Filter {
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//    }
-//
-//    @Override
-//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletRequest req = (HttpServletRequest) servletRequest;
-//        User user =(User) req.getSession().getAttribute("user");
-//
-//        if (user==null){
-//            HttpServletResponse resp=(HttpServletResponse)servletResponse;
-//            resp.sendRedirect("/");
-//        }else{
-//            filterChain.doFilter(servletRequest,servletResponse);
-//        }
-//    }
-//
-//    @Override
-//    public void destroy() {
-//    }
-//}
+@WebServlet(urlPatterns = "/books/**")
+public class UserFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        User user =(User) req.getSession().getAttribute("user");
+
+        if (user==null){
+            HttpServletResponse resp=(HttpServletResponse)servletResponse;
+            resp.sendRedirect("/");
+        }else{
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
