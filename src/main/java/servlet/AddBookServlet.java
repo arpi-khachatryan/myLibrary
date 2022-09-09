e01ed11218d24e3789d11deb8e9633c57a8aac1c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-09 06:36:29 +0400

    AddBookServlet

--- src/main/java/servlet/AddBookServlet.java
+++ src/main/java/servlet/AddBookServlet.java
@@ -7,17 +7,25 @@ import model.Book;
 import model.Description;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/books/add")
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 1,
+        maxFileSize = 1024 * 1024 * 10,
+        maxRequestSize = 1024 * 1024 * 100
+)
 public class AddBookServlet extends HttpServlet {
     private BookManager bookManager = new BookManager();
     private AuthorManager authorManager = new AuthorManager();
+    private static final String imagePath = "/Users/annakhachatryan/Library/Application Support/JetBrains/myLibrary/projectImages/";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -33,11 +41,19 @@ public class AddBookServlet extends HttpServlet {
         double price = Double.parseDouble(req.getParameter("price"));
         int authorId = Integer.parseInt(req.getParameter("author_id"));
         Author author = authorManager.getAuthorById(authorId);
+        Part profilePicPart = req.getPart("profilePic");
+        String fileName = null;
+        if (profilePicPart != null) {
+            long nanoTime = System.nanoTime();
+            fileName = nanoTime + "_" + profilePicPart.getSubmittedFileName();
+            profilePicPart.write(imagePath + fileName);
+        }
         Book book = Book.builder()
                 .title(title)
                 .description(Description.valueOf(description))
                 .price(price)
                 .author(author)
+                .profilePic(fileName)
                 .build();
         bookManager.addBook(book);
         resp.sendRedirect("/books");
