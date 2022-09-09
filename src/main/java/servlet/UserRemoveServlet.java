adb11fd84187238d512c0711e0585a28c8adfd5a
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-09 22:22:07 +0400

    UserRemoveServlet

---
+++ src/main/java/servlet/UserRemoveServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import manager.UserManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/users/remove")
+public class UserRemoveServlet extends HttpServlet {
+    private UserManager userManager = new UserManager();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        userManager.deleteUserById(id);
+        resp.sendRedirect("/users");
+    }
+}
